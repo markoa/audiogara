e74730f98030394e699dfdcd44a1cc8a21b1227c
Marko Anastasov <marko@renderedtext.com>
2010-12-25 19:09:38 +0100

    Blank Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Audiogara::Application.config.secret_token = '6f4306dbbe1e1760361660900d3f1ebe907fa702d5590148016c477b63579dbcd4ef2b0c6dfc89f6e8ad0c99db7b92dc75a34b7a7f99afc7f725fa8ba549224f'
