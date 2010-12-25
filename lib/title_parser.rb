8b1149a57694f5bb7907741bd895eddb9757a7ae
Marko Anastasov <marko@renderedtext.com>
2010-12-25 19:36:35 +0100

    TitleParser.

---
+++ lib/title_parser.rb
@@ -0,0 +1,52 @@
+class TitleParser
+
+  include ActiveSupport::Inflector
+
+  def self.parse(t)
+    parts = t.split("-").map(&:strip)
+
+    return {} if parts.size == 1
+
+    artist = remove_junk_from parts[0]
+    album = remove_junk_from parts[1]
+    
+    # album may be in one of the parts after the second
+    if album.blank? and parts.size > 2
+      2.upto(parts.size-1).each do |i|
+        album = remove_junk_from parts[i]
+        break if album.present?
+      end
+    end
+
+    return {} if artist.size == 0 or album.size == 0
+    return {} if ["va", "various artists"].include?(artist)
+    return {} if artist.include?("collection")
+    return {} if album.include?("soundtrack")
+    return {} if album.include?("discography")
+
+    { :artist => artist.titleize, :album => album.titleize }
+  end
+
+  protected
+
+  def self.remove_junk_from(t)
+    tmp = t
+
+    # if there's a parenthesis, remove everything starting from its position
+    ["(", "[", "{"].each do |parenthesis|
+      pos = tmp.index(parenthesis)
+      tmp = tmp.slice(0, pos) if not pos.nil?
+    end
+
+    # replace h4cky whitespace
+    [".", "_"].each do |ugly_separator|
+      tmp.gsub!(ugly_separator, " ")
+    end
+
+    # omit year
+    tmp.gsub!(/\d\d\d\d/, "")
+
+    tmp.strip.downcase
+  end
+
+end
---
+++ spec/lib/title_parser_spec.rb
@@ -0,0 +1,104 @@
+require 'spec_helper'
+require 'title_parser'
+
+describe TitleParser do
+
+  it "knows 'The Beatles - Revolver'" do
+    title = TitleParser.parse("The Beatles - Revolver")
+
+    title[:artist].should == "The Beatles"
+    title[:album].should == "Revolver"
+  end
+
+  it "knows 'Australian Crawl - The Boys Light Up (1980)'" do
+    title = TitleParser.parse("Australian Crawl - The Boys Light Up (1980)")
+
+    title[:artist].should == "Australian Crawl"
+    title[:album].should == "The Boys Light Up"
+  end
+  
+  it "knows 'The Sorrow - The Sorrow [2010]'" do
+    title = TitleParser.parse("The Sorrow - The Sorrow [2010]")
+
+    title[:artist].should == "The Sorrow"
+    title[:album].should == "The Sorrow"
+  end
+
+  it "knows 'Lil.Wayne - Hour.Past.Paid-(Bootleg)-2010-[NoFS]'" do
+    title = TitleParser.parse("Lil.Wayne - Hour.Past.Paid-(Bootleg)-2010-[NoFS]")
+
+    title[:artist].should == "Lil Wayne"
+    title[:album].should == "Hour Past Paid"
+  end
+
+  it "knows 'Flo Rida - Come With Me (New RnB)~Struzzin~'" do
+    title = TitleParser.parse("Flo Rida - Come With Me (New RnB)~Struzzin~")
+
+    title[:artist].should == "Flo Rida"
+    title[:album].should == "Come With Me"
+  end
+
+  it "knows 'Devlin - Bud, Sweat And Beers - 2010'" do
+    title = TitleParser.parse("Devlin - Bud, Sweat And Beers - 2010")
+
+    title[:artist].should == "Devlin"
+    title[:album].should == "Bud, Sweat And Beers"
+  end
+
+  it "knows 'Machinae Supremacy -2010- A View From The End Of The World'" do
+    title = TitleParser.parse("Machinae Supremacy -2010- A View From The End Of The World")
+
+    title[:artist].should == "Machinae Supremacy"
+    title[:album].should == "A View From The End Of The World"
+  end
+
+  it "knows 'Massakren (USA) - 2010 - Immersed In Chaos'" do
+    title = TitleParser.parse("Massakren (USA) - 2010 - Immersed In Chaos")
+
+    title[:artist].should == "Massakren"
+    title[:album].should == "Immersed In Chaos"
+  end
+
+  it "knows 'Reidar Larsen - Mr Blues Is Coming Twn - The Very Best Of (2003)'" do
+    title = TitleParser.parse("Reidar Larsen - Mr Blues Is Coming Twn - The Very Best Of (2003)")
+
+    title[:artist].should == "Reidar Larsen"
+    title[:album].should == "Mr Blues Is Coming Twn"
+  end
+
+  it "knows 'Dethklok - Season 3 Songs'" do
+    title = TitleParser.parse("Dethklok - Season 3 Songs")
+
+    title[:artist].should == "Dethklok"
+    title[:album].should == "Season 3 Songs"
+  end
+
+  it "skips 'The best Of Pantera CD'" do
+    title = TitleParser.parse("The best Of Pantera CD")
+    title.should == {}
+  end
+
+  it "skips Various Artists" do
+    title = TitleParser.parse("VA - Now that's what I call music")
+    title.should == {}
+
+    va_title = TitleParser.parse("Various Artists - Now that's what I call music")
+    va_title.should == {}
+  end
+
+  it "skips collections" do
+    title = TitleParser.parse("MC Stevie Hyper D MP3 Collection - Lounge MP3]")
+    title.should == {}
+  end
+
+  it "skips soundtracks" do
+    title = TitleParser.parse("Assassin's Creed: Brotherhood - Soundtrack")
+    title.should == {}
+  end
+
+  it "skips discographies" do
+    title = TitleParser.parse("Jefferson Airplane - Discography")
+    title.should == {}
+  end
+
+end
