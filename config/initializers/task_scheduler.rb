300c87fbcb58046731de41305f0761c89c9f82ed
Marko Anastasov <marko@renderedtext.com>
2010-12-26 22:02:24 +0100

    Add similar artists processing to task scheduler.

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -5,4 +5,6 @@ scheduler = Rufus::Scheduler.start_new
 scheduler.every "20m"  do
   FeedFetcher.fetch_and_save_torrents_from_tpb
   Torrent.process_artists
+  Artist.fetch_similar_artists
+  SimilarArtist.link_with_known_artists
 end
