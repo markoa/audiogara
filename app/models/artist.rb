5039b7c018d9eb7fb381c478d1ef3c5a8a7e6696
Marko Anastasov <marko@renderedtext.com>
2010-12-26 13:32:16 +0100

    Artist.create_from_hash

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -5,4 +5,17 @@ class Artist < ActiveRecord::Base
   validates_presence_of :name
   validates_presence_of :lastfm_url
 
+  def self.create_from_hash(hash)
+    return nil if hash.empty?
+
+    create(:name => hash[:name],
+           :mbid => hash[:mbid],
+           :lastfm_url => hash[:url],
+           :image_small_url => hash[:image_small],
+           :image_medium_url => hash[:image_medium],
+           :image_large_url => hash[:image_large],
+           :image_extralarge_url => hash[:image_extralarge],
+           :image_mega_url => hash[:image_mega])
+  end
+
 end
--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -7,4 +7,38 @@ describe Artist do
 
   it { should have_many(:torrents) }
 
+  describe ".create_from_hash" do
+    
+    before do
+      @hash = {
+        :mbid=>"39ab1aed-75e0-4140-bd47-540276886b60",
+        :image_small=>"http://userserve-ak.last.fm/serve/34/44937531.jpg",
+        :image_medium=>"http://userserve-ak.last.fm/serve/64/44937531.jpg",
+        :url=>"http://www.last.fm/music/Oasis",
+        :image_large=>"http://userserve-ak.last.fm/serve/126/44937531.jpg",
+        :image_extralarge=>"http://userserve-ak.last.fm/serve/252/44937531.jpg",
+        :name=>"Oasis",
+        :image_mega=>"http://userserve-ak.last.fm/serve/_/44937531/Oasis+Maine+Road.jpg"
+      }
+    end
+
+    it "creates a new record from a hash from Last.fm" do
+      expect { Artist.create_from_hash(@hash) }.to change(Artist, :count).by(1)
+
+      artist = Artist.last
+      artist.name.should == "Oasis"
+      artist.mbid.should == @hash[:mbid]
+      artist.lastfm_url.should == @hash[:url]
+      artist.image_small_url.should == @hash[:image_small]
+      artist.image_medium_url.should == @hash[:image_medium]
+      artist.image_large_url.should == @hash[:image_large]
+      artist.image_extralarge_url.should == @hash[:image_extralarge]
+      artist.image_mega_url.should == @hash[:image_mega]
+    end
+
+    it "doesn't create a new record from an empty hash" do
+      expect { Artist.create_from_hash({}) }.to change(Artist, :count).by(0)
+    end
+  end
+
 end
