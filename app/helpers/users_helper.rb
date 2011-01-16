566f7773e96cf962e45a322cba0aae2529dc69d9
Marko Anastasov <marko@renderedtext.com>
2011-01-16 14:12:56 +0100

    User real name, avatar on profile page.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,4 +3,8 @@ module UsersHelper
   def user_has_too_few_known_interests?
     @known_interests_count < Interest::USEFULNESS_THRESHOLD
   end
+
+  def user_name
+    @user.realname.present? ? @user.realname.split.first : @user.lastfm_username
+  end
 end
